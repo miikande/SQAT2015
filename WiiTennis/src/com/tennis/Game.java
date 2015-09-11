a8332d26c03de8ad400a6d12b8558a88a837b706
Miikka Andersson <miikka.andersson@gmail.com>
2015-09-11 19:25:42 +0300

    Added initial Player and Game classes + tests

---
+++ WiiTennis/src/com/tennis/Game.java
@@ -0,0 +1,106 @@
+package com.tennis;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This class represents a single game of a tennis set/match.
+ * 
+ * @author Miikka Andersson
+ *
+ */
+public class Game {
+
+	/*
+	 * Two game modes supported:
+	 *   two player game: single
+	 *   four player game: doubles
+	 */
+	public static final int GAME_MODE_SINGLE = 2;
+	public static final int GAME_MODE_DOUBLES = 4;
+	
+	// Teams
+	public static final int TEAM_1 = 0;
+	public static final int TEAM_2 = 1;
+	
+	// Keeping points for both teams (idx0=team1, idx1=team2)
+	private Integer[] points;
+	
+	// We'll hold our players in a list to be able to serve both two and four player games
+	private List<Player> players;
+	
+	/**
+	 * Creates a new game with given players (either two or four
+	 * player game). In two player game, players in first and second
+	 * indexes are opponents (team1 and team2). However, in four 
+	 * player mode indexes 0 and 1 are on the same side (team1). And 
+	 * the same goes for indexes 2 & 3 (team2).  
+	 * 
+	 * @param players
+	 */
+	public Game(Player... players) {
+		
+		// We'll accept either two or four players
+		if (players.length != 2 && players.length != 4) {
+			throw new IllegalArgumentException("Must provide either two or four players!");
+		}
+		
+		this.players = new LinkedList<Player>(Arrays.asList(players));
+		
+		// Initialize points for both teams
+		points = new Integer[]{0, 0};
+	}
+
+	/**
+	 * Returns used game mode: single or doubles
+	 * @return 2 for single game, 4 in case of doubles
+	 */
+	public int getGameMode() {
+		return players.size();
+	}
+	
+	/**
+	 * Returns players belonging to given team
+	 * @param integer of team (TEAM_1 or TEAM_2)
+	 * @return a list of players
+	 */
+	public List<Player> getTeam(int team) {
+		validateTeam(team);
+		
+		if (players.size() == 2)
+			return Arrays.asList(players.get(team));
+		
+		// If we reached this point it means this is a four player game!
+		if (team == TEAM_1) {
+			return Arrays.asList(players.get(0), players.get(1));
+		}
+		
+		return Arrays.asList(players.get(2), players.get(3));
+	}
+	
+	/**
+	 * Adds a point to given team
+	 * @param team
+	 */
+	public void addPoint(int team) {
+		validateTeam(team);
+		points[team]++;		
+	}
+	
+	private void validateTeam(int team) {
+		if (team != TEAM_1 && team != TEAM_2) {
+			throw new IllegalArgumentException(
+					"Unknown team, use either " + TEAM_1 + " or " +
+					TEAM_2 + ", was: " + team);
+		}
+	}
+
+	public int getPoints(int team) {
+		validateTeam(team);
+		
+		return points[team];
+	}
+	
+}
---
+++ WiiTennis/src/com/tennis/tests/TestGame.java
@@ -0,0 +1,80 @@
+package com.tennis.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tennis.Game;
+import com.tennis.Player;
+
+public class TestGame {
+	
+	private Player player1;
+	private Player player2;
+	private Game game;
+	
+	@Before
+	public void init() {
+		player1 = new Player("Ronald McBurger");
+		player2 = new Player("Tony Tiger");
+	}
+	
+	private void initTwoPlayerGame() {
+		// Create a game with two players
+		game = new Game(new Player[] {player1, player2});
+	}
+	
+	@Test
+	public void testAddPointsToTeams() {
+		initTwoPlayerGame(); 
+
+		// Should be zero in the beginning...
+		assertTrue(game.getPoints(Game.TEAM_1) == 0);
+		assertTrue(game.getPoints(Game.TEAM_2) == 0);
+		
+		// Add one point for team 1
+		game.addPoint(Game.TEAM_1);
+		
+		// And two points for team 2
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		
+		// Team1 has one point?
+		assertTrue(game.getPoints(Game.TEAM_1) == 1);
+		
+		// And team2 two points?
+		assertTrue(game.getPoints(Game.TEAM_2) == 2);
+	}
+
+	@Test
+	public void testGameInitializesWithTwoPlayers() {
+		initTwoPlayerGame();
+		
+		// Let's ensure our game has two players...
+		assertTrue(game.getGameMode() == Game.GAME_MODE_SINGLE);
+	}
+	
+	@Test
+	public void testBothTeamsHasPlayer() {
+		initTwoPlayerGame();
+		
+		// Ensure we have a player in team1
+		assertTrue(game.getTeam(Game.TEAM_1).size() == 1);
+
+		// And in team2 as well..
+		assertTrue(game.getTeam(Game.TEAM_2).size() == 1);
+	}
+	
+	@Test
+	public void testPlayersAreInRightTeams() {
+		initTwoPlayerGame();
+		
+		Player p1 = game.getTeam(Game.TEAM_1).get(0);
+		assertEquals(player1.getName(), p1.getName());
+		
+		Player p2 = game.getTeam(Game.TEAM_2).get(0);
+		assertEquals(player2.getName(), p2.getName());
+	}
+
+}
