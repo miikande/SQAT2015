1e1def11bdd7c750846d45592faf701903294be0
Miikka Andersson <miikka.andersson@gmail.com>
2015-09-12 10:31:58 +0300

    Added scoring and game status functionalities

--- WiiTennis/src/com/tennis/Game.java
+++ WiiTennis/src/com/tennis/Game.java
@@ -1,6 +1,5 @@
 package com.tennis;
 
-import java.security.InvalidParameterException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,6 +24,24 @@ public class Game {
 	public static final int TEAM_1 = 0;
 	public static final int TEAM_2 = 1;
 	
+	/*
+	 * Scores according to specification:
+	 * 
+	 *   Scores from zero to three points are described as 
+	 *   "love", "fifteen", "thirty", and "forty"
+	 */
+	public static final String SCORE_ZERO 		= "Love";
+	public static final String SCORE_FIFTEEN 	= "Fifteen";
+	public static final String SCORE_THIRTY		= "Thirty";
+	public static final String SCORE_FORTY 		= "Forty";
+	
+	// Game statuses
+	public static final int GAME_STATUS_ONGOING 		 = 0;
+	public static final int GAME_STATUS_TEAM_1_WON 		 = 1;
+	public static final int GAME_STATUS_TEAM_2_WON 		 = 2;
+	public static final int GAME_STATUS_TEAM_1_ADVANTAGE = 3;
+	public static final int GAME_STATUS_TEAM_2_ADVANTAGE = 4;
+	
 	// Keeping points for both teams (idx0=team1, idx1=team2)
 	private Integer[] points;
 	
@@ -47,6 +64,12 @@ public class Game {
 			throw new IllegalArgumentException("Must provide either two or four players!");
 		}
 		
+		// No null values accepted
+		for (int i = 0; i < players.length; i++) {
+			if (players[i] == null)
+				throw new IllegalArgumentException("Null values are not accepted as parameters!");
+		}
+		
 		this.players = new LinkedList<Player>(Arrays.asList(players));
 		
 		// Initialize points for both teams
@@ -66,18 +89,21 @@ public class Game {
 	 * @param integer of team (TEAM_1 or TEAM_2)
 	 * @return a list of players
 	 */
-	public List<Player> getTeam(int team) {
+	public List<Player> getPlayersForTeam(int team) {
 		validateTeam(team);
+		List<Player> teamMembers;
 		
-		if (players.size() == GAME_MODE_SINGLE)
-			return Arrays.asList(players.get(team));
-		
-		// If we reached this point it means this is a four player game!
-		if (team == TEAM_1) {
-			return Arrays.asList(players.get(0), players.get(1));
+		if (players.size() == GAME_MODE_SINGLE) {
+			teamMembers = Arrays.asList(players.get(team));
+		}
+		else if (team == TEAM_1) {
+			teamMembers = Arrays.asList(players.get(0), players.get(1));
+		}
+		else {
+			teamMembers = Arrays.asList(players.get(2), players.get(3));
 		}
 		
-		return Arrays.asList(players.get(2), players.get(3));
+		return teamMembers;
 	}
 	
 	/**
@@ -113,6 +139,75 @@ public class Game {
 		validateTeam(team);
 		
 		return points[team];
+	}
+
+	/**
+	 * Returns given team's score in string format.
+	 * First numbers are in textual format whereas rest
+	 * are in digits in increment of ten.
+	 * @param team id
+	 * @return team's score as a String
+	 */
+	public String getScore(int team) {
+		validateTeam(team);
+		String score;
+		
+		switch (points[team]) {
+		case 0:
+			score = SCORE_ZERO;
+			break;
+			
+		case 1:
+			score = SCORE_FIFTEEN;
+			break;
+			
+		case 2:
+			score = SCORE_THIRTY;
+			break;
+			
+		case 3:
+			score = SCORE_FORTY;
+			break;
+
+		default:
+			/* 
+			 * We'll need to calculate the score. The first three points
+			 * are worth of forty points and rest are 10pts each.
+			 */
+			score = Integer.toString(40 + (10 * (points[team] - 3)));
+		}
+		
+		return score;
+	}
+
+	/**
+	 * Returns status of this game. Status can be:
+	 *   1) Ongoing
+	 *   2) Team 1 won
+	 *   3) Team 2 won
+	 *   4) Team 1 has advantage
+	 *   5) Team 2 has advantage
+	 *   
+	 * Specification says:
+	 *   A game is won by the first player to have won at least four 
+	 *   points in total and at least two points more than the opponent.
+	 *   
+	 * @return status code as an integer
+	 */
+	public int getGameStatus() {
+		int status = GAME_STATUS_ONGOING;
+		
+		// Check if either team has won this game
+		if (points[TEAM_1] >= 4 || points[TEAM_2] >= 4) {
+			if ((points[TEAM_1] - points[TEAM_2]) >= 2) {
+				status = GAME_STATUS_TEAM_1_WON;
+			}
+			else if ((points[TEAM_2] - points[TEAM_1]) >= 2) {
+				status = GAME_STATUS_TEAM_2_WON;
+			}
+		}
+		
+		return status;
 	}
 	
 }
--- WiiTennis/tests/com/tennis/TestGame.java
+++ WiiTennis/tests/com/tennis/TestGame.java
@@ -60,21 +60,156 @@ public class TestGame {
 		initTwoPlayerGame();
 		
 		// Ensure we have a player in team1
-		assertTrue(game.getTeam(Game.TEAM_1).size() == 1);
+		assertTrue(game.getPlayersForTeam(Game.TEAM_1).size() == 1);
 
 		// And in team2 as well..
-		assertTrue(game.getTeam(Game.TEAM_2).size() == 1);
+		assertTrue(game.getPlayersForTeam(Game.TEAM_2).size() == 1);
 	}
 	
 	@Test
 	public void testPlayersAreInRightTeams() {
 		initTwoPlayerGame();
 		
-		Player p1 = game.getTeam(Game.TEAM_1).get(0);
+		Player p1 = game.getPlayersForTeam(Game.TEAM_1).get(0);
 		assertEquals(player1.getName(), p1.getName());
 		
-		Player p2 = game.getTeam(Game.TEAM_2).get(0);
+		Player p2 = game.getPlayersForTeam(Game.TEAM_2).get(0);
 		assertEquals(player2.getName(), p2.getName());
 	}
 
+	@Test
+	public void testPointToScoreConversion() {
+		initTwoPlayerGame(); 
+		
+		/* 
+		 * We should test according to spec:
+		 * scores from zero to three points are described as
+		 * "love", "fifteen", "thirty", and "forty"
+		 */
+		assertEquals(Game.SCORE_ZERO, game.getScore(Game.TEAM_1));
+		
+		// First point
+		game.addPoint(Game.TEAM_1);
+		assertEquals(Game.SCORE_FIFTEEN, game.getScore(Game.TEAM_1));
+		
+		// Second one
+		game.addPoint(Game.TEAM_1);
+		assertEquals(Game.SCORE_THIRTY, game.getScore(Game.TEAM_1));
+		
+		// And third point
+		game.addPoint(Game.TEAM_1);
+		assertEquals(Game.SCORE_FORTY, game.getScore(Game.TEAM_1));
+		
+		/* 
+		 * Next we'll test some numeric values up to, say, 120 (there 
+		 * are no limits set by the system)... 
+		 */
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("50", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("60", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("70", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("80", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("90", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("100", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("110", game.getScore(Game.TEAM_1));
+		
+		game.addPoint(Game.TEAM_1);
+		assertEquals("120", game.getScore(Game.TEAM_1));
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructorExceptionHandling1Param() {
+		game = new Game(new Player[] {player1}); 
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructorExceptionHandling3Param() {
+		game = new Game(new Player[] {player1, player1, player1}); 
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructorExceptionHandling5Param() {
+		game = new Game(new Player[] {player1, player1, player1, player1, player1}); 
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructorExceptionHandlingEmptyParam() {
+		game = new Game(new Player[] {}); 
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructorExceptionHandlingNullParam() {
+		game = new Game(new Player[] {null, null}); 
+	}
+	
+	@Test
+	public void testGameStatusTeam1Won() {
+		initTwoPlayerGame();
+		
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_1);
+		
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		
+		assertEquals(Game.GAME_STATUS_TEAM_1_WON, game.getGameStatus());
+	}
+	
+	@Test
+	public void testGameStatusTeam2Won() {
+		initTwoPlayerGame();
+		
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_1);
+		
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		
+		assertEquals(Game.GAME_STATUS_TEAM_2_WON, game.getGameStatus());
+	}
+	
+	@Test
+	public void testGameStatusOngoing() {
+		initTwoPlayerGame();
+		assertEquals(Game.GAME_STATUS_ONGOING, game.getGameStatus());
+		
+		// Let's add some points and check if the game is still ongoing (as it should)
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_1);
+		game.addPoint(Game.TEAM_2);
+		game.addPoint(Game.TEAM_2);
+		assertEquals(Game.GAME_STATUS_ONGOING, game.getGameStatus());
+	}
+	
+	@Test
+	public void testGameStatusTeam1Advantage() {
+		fail();
+	}
+	
+	@Test
+	public void testGameStatusTeam2Advantage() {
+		fail();
+	}
 }
